a760b8335ab5f7ed3d79a92c3cfdd2ccd6cc319a
谭尧丹 <tanyaodan@qq.com>
2021-10-26 21:06:23 +0800

    Create Caesar.py

---
+++ ADWorld/Caesar.py
@@ -0,0 +1,15 @@
+text = 'oknqdbqmoq{kag_tmhq_xqmdzqp_omqemd_qzodkbfuaz}' # 凯撒密码
+flag = ''
+for i in range(1, 27):
+    s = ''
+    for x in text:
+        if x.isalpha():
+            s += chr(ord('a')+(ord(x)-i)%26)
+        else:
+            s += x
+    s = s.lower()
+    if 'cyberpeace' in s:
+        flag = s
+    print('{}的移位是{}'.format(s, (ord(text[0])-ord(s[0]))%26))
+
+print(flag) # cyberpeace{you_have_learned_caesar_encryption}
