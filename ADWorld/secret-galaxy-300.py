af4247d54c0fe8e1479a7dbff253009d995e28b4
谭尧丹 <tanyaodan@qq.com>
2022-03-11 20:25:24 +0800

    Create secret-galaxy-300.py

---
+++ ADWorld/secret-galaxy-300.py
@@ -0,0 +1,26 @@
+flag = ''
+off_601268 = 'Andromeda'
+off_601270 = 'Messier'
+off_601278 = 'Sombrero'
+off_601280 = 'Triangulum'
+flag += off_601268[8]
+flag += off_601280[7]
+flag += off_601270[4]
+flag += off_601268[6]
+flag += off_601268[1]
+flag += off_601270[2]
+flag += chr(95)
+flag += off_601268[8]
+flag += off_601268[3]
+flag += off_601278[5]
+flag += chr(95)
+flag += off_601268[8]
+flag += off_601268[3]
+flag += off_601268[4]
+flag += off_601280[6]
+flag += off_601280[4]
+flag += off_601268[2]
+flag += chr(95)
+flag += off_601280[6]
+flag += off_601270[3]
+print(flag)
