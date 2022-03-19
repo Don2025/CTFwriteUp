071d5444e0ff8dffa9bcd24d4dadc4d7aadcaf16
谭尧丹 <tanyaodan@qq.com>
2022-03-19 16:26:30 +0800

    Create 迟来的签到题.py

---
+++ PwnTheBox/迟来的签到题.py
@@ -0,0 +1,9 @@
+from base64 import *
+
+s = b64decode('AAoHAR1XICciX1IlXiBUVFFUIyRRJFRQVyUnVVMnUFcgIiNXXhs=')
+for i in range(0, 256):
+    ans = ''
+    for x in s:
+        ans += chr(x^i)
+    if "flag" in ans:
+        print(ans)
