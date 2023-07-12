fc917db68f745a2a9e23c64551ed861833f6c1af
t0ur1st <tanyaodan@qq.com>
2023-07-12 10:16:15 +0800

    Update 00_angr_find.py

--- Angr_CTF/00_angr_find.py
+++ Angr_CTF/00_angr_find.py
@@ -1,11 +1,14 @@
 import angr
 
 project = angr.Project('./00_angr_find')
-initial_state = project.factory.entry_state()
+initial_state = project.factory.entry_state(add_options={
+        angr.options.SYMBOL_FILL_UNCONSTRAINED_MEMORY,
+        angr.options.SYMBOL_FILL_UNCONSTRAINED_REGISTERS})
 simulation = project.factory.simgr(initial_state)
 simulation.explore(find=0x8048678)
 if simulation.found:
     solution_state = simulation.found[0]
-    print(solution_state.posix.dumps(0))
+    passwd = solution_state.posix.dumps(0).decode()
+    print('[+] Congratulations! Solution is: %s' % passwd)
 else:
     raise Exception('Could not find the solution')
